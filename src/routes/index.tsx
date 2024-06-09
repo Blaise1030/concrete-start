ed830f76dbaf9d8881da03d6ec0fe5351ef09201
blaise <blaise.t@rocketbots.io>
2024-06-09 20:22:22 +0800

    feat: added login page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,18 @@
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
+import { Icon } from "@iconify-icon/solid";
+import { createSignal } from "solid-js";
 
 export default function Home() {
   return (
     <div class="w-screen h-screen flex flex-row">
-      <section class="bg-primary h-full w-[50%] lg:block hidden">
-        <div class="flex flex-col text-background mx-auto sm:p-6 md:p-8 h-full">
+      <section class="bg-black h-full w-[50%] lg:block hidden relative border-e">
+        <img
+          src="/background.jpeg"
+          alt="background"
+          class="w-full h-full absolute object-cover opacity-50"
+        />
+        <div class="flex flex-col mx-auto sm:p-6 md:p-8 h-full absolute text-white">
           <header>
             <h1 class="text-xl tracking-tighter font-semibold">⌘ Acme Inc.</h1>
           </header>
@@ -19,18 +26,47 @@ export default function Home() {
           </blockquote>
         </div>
       </section>
-      <section class="bg-background h-full w-[50%] flex items-center">
-        <div class="mx-auto max-w-md w-full flex flex-col space-y-4">
-          <header class="flex flex-col space-y-2">
-            <h2>Sign In</h2>
-          </header>
-          <Input placeholder="abc@email.com" />
-          <Input placeholder="P@ssw0rd" />
-          <Button>Sign In</Button>
-
-          <Button variant={"outline"}>Google</Button>
-        </div>
+      <section class="bg-background h-full lg:w-[50%] w-full flex items-center dark:bg-grid-white/5 bg-grid-black/5">
+        <AuthenticationForm />
       </section>
     </div>
   );
 }
+
+function AuthenticationForm() {
+  const [isLogin, setIsLogin] = createSignal(true);
+  return (
+    <>
+      <div class="p-2 md:p-8 top-0 right-0 fixed">
+        <Button variant={"link"} onClick={() => setIsLogin((a) => !a)}>
+          {isLogin() ? "Register" : "Sign In"}
+        </Button>
+      </div>
+      <div class="mx-auto max-w-sm w-full flex flex-col space-y-4 ">
+        <header class="flex flex-col">
+          <h2 class="font-bold text-2xl">
+            {isLogin() ? "Welcome Back!" : "Join Us!"}
+          </h2>
+          <p class="text-muted-foreground">
+            {isLogin() ? "Log In to Your Account" : "Create Your Free Account"}
+          </p>
+        </header>
+        <Input class="bg-background" placeholder="abc@email.com" />
+        <Input class="bg-background" placeholder="P@ssw0rd" type="password" />
+        <Button>{isLogin() ? "Sign In" : "Register"}</Button>
+        <div class="pb-2">
+          <div class="border-b items-center justify-center flex">
+            <div class="text-xs text-muted-foreground w-fit -mb-[10px] bg-background px-2">
+              OR CONTINUE WITH
+            </div>
+          </div>
+        </div>
+        <Button variant={"outline"} class="bg-background">
+          <Icon icon="mdi:google" class="mr-2" />
+          Google
+        </Button>
+      </div>
+    </>
+  );
+}
+// Join Us! Create Your Free Account
