b590286f1112c9fcc6235b802598f338866463d5
BlaiseTiong1030 <blaise.t@rocketbots.io>
2024-07-04 23:22:14 +0800

    feat: added protected route middleware

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,12 +1,17 @@
 import { redirect } from "@solidjs/router";
 import { createMiddleware } from "@solidjs/start/middleware";
 import { lucia } from "~/backend/lucia";
+import URLPattern from "url-pattern"
+
+const protectedPath = [
+  '/dashboard*',
+]
 
 export default createMiddleware({
   onRequest: [
     async (event) => {
       const pathname = new URL(event.request.url)?.pathname
-      if (pathname === '/dashboard') {
+      if (protectedPath?.some((pattern) => (new URLPattern(pattern).match(pathname)))) {
         const cookieHeader = event.request.headers.get('Cookie')
         const sessionId = lucia.readSessionCookie(cookieHeader ?? "");
         if (!sessionId) return redirect(`/login?path=${pathname ?? "/"}`)
